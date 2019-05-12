58e535cde90f4d1bce005fa68bdd913e15276797
Marek Ptak <marekptak@aol.de>
2019-05-12 21:28:44 +0200

    nowe elementy – do przemyślenia czy tak to ma być czy coś zmienić. Dodany nowy projekt – aplikacja internetowa platformy ASP.NET Core – aplikacja internetowa MVC (bez uwierzytelniania, .net core 2.1) Tworzenie modelu: Models → klasa ksiazka Models → klasa autor Models → klasa rodzajnosnika relacje między tabelami – to było dziś i była mowa żeby zrobić virtual ale czy to ma być tak: public virtual int Id { get; set; } w wwwroot wstawiłem przykładowe okładki i plik tekstowy z przykładowymi danymi.

---
+++ DeepLib/Models/Autor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeepLib.Models
+{
+    public class Autor
+    {
+        public virtual int Id { get; set; }
+        public string ImieINazwisko { get; set; }
+   
+    }
+}
---
+++ DeepLib/Models/Ksiazka.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeepLib.Models
+{
+    public class Ksiazka
+    {
+        public int Id { get; set; }
+        public string Tytul { get; set; }
+        public int RokWydania { get; set; }
+        //public int RodzajNosnika { get; set; }
+        //public int Autor { get; set; }
+        public string Okladka { get; set; }
+        public bool Status { get; set; }
+        public string KtoZmienilStatus { get; set; }
+        public string Komentarz { get; set; }
+        public DateTime KiedyPozyczyl { get; set; }
+        
+        public virtual int Autor { get; set; }
+        public virtual int RodzajNosnika { get; set; }
+
+
+    }
+}
