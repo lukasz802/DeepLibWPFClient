a1bf67fcbedfb9f8d58e7ca2fb9cc718f9d750dd
Lukasz A <lukasz.amroziewicz@gmail.com>
2019-05-12 13:21:49 +0200

    Dodaj pliki projektów.

---
+++ DeepLibClient/Contols/SearchTextBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeepLibClient.Contols
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SearchTextBox.xaml
+    /// </summary>
+    public partial class SearchTextBox : UserControl
+    {
+        public SearchTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchTextBoxControl_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (((TextBox)sender).Text != "") { DefaultSearchLabelControl.Visibility = Visibility.Hidden; }
+            else { DefaultSearchLabelControl.Visibility = Visibility.Visible; }
+        }
+    }
+}
---
+++ DeepLibClient/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+
+namespace DeepLibClient
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainPanelImage_Load;
+            MusicAlbumRadioButton.Checked += MenuContentButton_Checked;
+            MusicTitleRadioButton.Checked += MenuContentButton_Checked;
+            MusicPerformerRadioButton.Checked += MenuContentButton_Checked;
+            MovieDirectorRadioButton.Checked += MenuContentButton_Checked;
+            MovieTitleRadioButton.Checked += MenuContentButton_Checked;
+            MovieYearRadioButton.Checked += MenuContentButton_Checked;
+            BookAuthorRadioButton.Checked += MenuContentButton_Checked;
+            BookTitleRadioButton.Checked += MenuContentButton_Checked;
+            BookYearRadioButton.Checked += MenuContentButton_Checked;
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == CloseButton) { this.Close(); }
+            else if (sender == MaximizeButton)
+            {
+                if (this.WindowState != WindowState.Maximized) { this.WindowState = WindowState.Maximized; }
+                else { this.WindowState = WindowState.Normal; }
+            }
+            else if (sender == MinimizeButton) { this.WindowState = WindowState.Minimized; }
+        }
+
+        private void MainPanelImage_Load(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int img_num = rnd.Next(1, 4);
+
+            MainPanelImage.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "\\IMG\\Image_" + img_num.ToString() + ".jpg", UriKind.Absolute));
+            MainPanelImage.Height = (int)SystemParameters.MaximizedPrimaryScreenHeight - 40;
+        }
+
+        private void MenuContentButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (((RadioButton)sender).IsChecked != false)
+            {
+                Point tempPoint = new Point(0, 0);
+                Point constPoint = new Point(0, 0);
+
+                if (MainTabControl.SelectedIndex == 2) { constPoint = MusicTitleRadioButton.TransformToAncestor(MainTabControl).Transform(tempPoint); }
+                else if (MainTabControl.SelectedIndex == 3) { constPoint = MovieTitleRadioButton.TransformToAncestor(MainTabControl).Transform(tempPoint); }
+                else if (MainTabControl.SelectedIndex == 1) { constPoint = BookTitleRadioButton.TransformToAncestor(MainTabControl).Transform(tempPoint); }
+
+                tempPoint.X = -constPoint.X;
+                tempPoint.Y = -constPoint.Y;
+                Point relativePoint = ((RadioButton)sender).TransformToAncestor(MainTabControl).Transform(tempPoint);
+                ThicknessAnimation a = new ThicknessAnimation { To = new Thickness(relativePoint.X, 0, 0, 0) };
+                DoubleAnimation d = new DoubleAnimation { To = ((RadioButton)sender).ActualWidth };
+                a.Duration = d.Duration = new Duration(TimeSpan.Parse("0:0:0.08"));
+
+                if (MainTabControl.SelectedIndex == 2)
+                {
+                    MusicMenuRectangle.BeginAnimation(WidthProperty, d);
+                    MusicMenuRectangle.BeginAnimation(MarginProperty, a);
+                }
+                else if (MainTabControl.SelectedIndex == 3)
+                {
+                    MovieMenuRectangle.BeginAnimation(WidthProperty, d);
+                    MovieMenuRectangle.BeginAnimation(MarginProperty, a);
+                }
+                else if (MainTabControl.SelectedIndex == 1)
+                {
+                    BookMenuRectangle.BeginAnimation(WidthProperty, d);
+                    BookMenuRectangle.BeginAnimation(MarginProperty, a);
+                }
+            }
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (((TextBox)sender).Text != "") { DefaultSearchLabel.Visibility = Visibility.Hidden; }
+            else { DefaultSearchLabel.Visibility = Visibility.Visible; }
+        }
+    }
+}
