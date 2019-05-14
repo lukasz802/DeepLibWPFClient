f2d939a4fe6fffd7b4dee68aa5fd852d259825cb
Marek Ptak <marekptak@aol.de>
2019-05-14 21:51:07 +0200

    W Modelu dodane poprawione klasy Author, Book i TypeOfMedium.

---
+++ DeepLib/Models/TypeOfMedium.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeepLib.Models
+{
+    [Table("Medium")]
+    public class TypeOfMedium
+    {
+        [Key]
+        public virtual int Id { get; set; }
+        public string Medium { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
